fd43787b2a7e6085492c4410bdb8538662021206
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2025-10-02 20:17:50 +0700

    c

--- modules.blade.php
+++ modules.blade.php
@@ -1,40 +1,115 @@
 <?php
 use_module([
-'download' => ['web'=>['auto_query'=>true]],
-'kepegawaian'=>['web'=>['detail'=>true],
-]
+  'download' => ['web' => ['auto_query' => true]],
+  'kepegawaian' => [
+    'web' => ['detail' => true],
+  ]
 ]);
 
-$config['option'] = ['Jadwal Rawat Jalan'=>[
-  ['Rawat Jalan Senin Kamis', 'text'],
-  ['Rawat Jalan Jumat', 'text'],
-  ['Rawat Jalan Sabtu', 'text']
-],
-'Jadwal Pendaftaran'=>[
-  ['Pendaftaran Senin Kamis', 'text'],
-  ['Pendaftaran Jumat', 'text'],
-  ['Pendaftaran Sabtu', 'text'],
-],
-'Jadwal Rawat Inap'=>[
-  ['Rawat Inap', 'text']
-]
-];
 
-if(!function_exists('logo_upt')){
-  function logo_upt($text){
+if (!function_exists('logo_upt')) {
+  function logo_upt($text)
+  {
     $prefix = "UPT Puskesmas";
-    
+
     // pastikan ada prefix "UPT Puskesmas"
     if (stripos($text, $prefix) === 0) {
-        $instansi = $prefix;
-        // ambil sisa kalimat setelah "UPT Puskesmas"
-        $lokasi = trim(substr($text, strlen($prefix)));
+      $instansi = $prefix;
+      // ambil sisa kalimat setelah "UPT Puskesmas"
+      $lokasi = trim(substr($text, strlen($prefix)));
     } else {
-        // fallback kalau format tidak sesuai
-        $instansi = $text;
-        $lokasi = '';
+      // fallback kalau format tidak sesuai
+      $instansi = $text;
+      $lokasi = '';
     }
 
-    return [$instansi,$lokasi];
+    return [$instansi, $lokasi];
   }
 }
+
+
+
+add_route('admin', [
+  'title' => 'Pendaftaran Anggota',
+  'name' => 'pendaftaran',
+  'icon' => 'fa-list',
+  'path' => 'pendaftaran',
+  'method' => ['get'],
+  'function' => 'index',
+  'controller' => \App\Http\Controllers\DataController::class,
+  'show_in_sidebar' => true,
+]);
+add_route('admin', [
+  'title' => 'Pendaftaran Guru',
+  'name' => 'pendaftaran.guru',
+  'icon' => 'fa-list',
+  'path' => 'pendaftaran-guru',
+  'method' => ['get'],
+  'function' => 'index',
+  'controller' => \App\Http\Controllers\DataController::class,
+  'show_in_sidebar' => true,
+]);
+add_route('admin', [
+  'title' => 'Pendaftaran Guru',
+  'name' => 'pendaftaran.guru',
+  'icon' => 'fa-list',
+  'path' => 'pendaftaran-guru',
+  'method' => ['get'],
+  'function' => 'index',
+  'controller' => \App\Http\Controllers\DataController::class,
+  'show_in_sidebar' => true,
+]);
+
+add_route('admin', [
+  'title' => 'Lihat',
+  'name' => 'pendaftaran',
+  'icon' => 'fa-list',
+  'path' => 'pendaftaran/show',
+  'method' => ['get'],
+  'function' => 'index',
+  'controller' => \App\Http\Controllers\DataController::class,
+  'show_in_sidebar' => false,
+]);
+
+add_route('public', [
+  'name' => 'register.user',
+  'path' => 'pendaftaran',
+  'method' => ['get'],
+  'function' => 'fungsisaya',
+  'controller' => \App\Http\Controllers\DataController::class,
+]);
+
+
+
+add_option('jadwal_pelayanan',
+  array(
+    ['Rawat Jalan', 'break'],
+    ['rawat_jalan_senin_kamis', 'text'],
+    ['rawat_jalan_jumat', 'text'],
+    ['rawat_jalan_sabtu', 'text'],
+    ['Pendaftaran', 'break'],
+    ['pendaftaran_senin_kamis', 'text'],
+    ['pendaftaran_jumat', 'text'],
+    ['pendaftaran_sabtu', 'text'],
+    ['IGD', 'text']
+  )
+);
+
+add_option(
+  'profile_puskesmas',
+  array(
+    ['Rawat Jalan', 'break'],
+    ['rawat_jalan_senin_kamis', 'text'],
+    ['rawat_jalan_jumat', 'text'],
+    ['rawat_jalan_sabtu', 'text'],
+    ['Pendaftaran', 'break'],
+    ['pendaftaran_senin_kamis', 'text'],
+    ['pendaftaran_jumat', 'text'],
+    ['pendaftaran_sabtu', 'text'],
+    ['IGD', 'text']
+  )
+);
+
+add_option('template_asset',[
+['background_header','file','image/png,image/jpeg']
+]);
